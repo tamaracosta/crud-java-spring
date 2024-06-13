f4a009f10f2729bbb515d7b7b547f653ea4fdd51
tamaracosta <tamarasantosc@gmail.com>
2024-06-13 11:32:52 -0300

    feat: controller (CRUD)

---
+++ src/main/java/com/example/crud/controllers/ProductController.java
@@ -0,0 +1,74 @@
+package com.example.crud.controllers;
+
+import com.example.crud.domain.product.Product;
+import com.example.crud.domain.product.ProductRepository;
+import com.example.crud.domain.product.RequestProduct;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+
+    @Autowired
+    private ProductRepository repository;
+
+    @GetMapping
+    public ResponseEntity getAllProducts(){
+        //var allProducts = repository.findAll(); como estava antes
+        //depois de criar o active true
+        var allProducts = repository.findAllByActiveTrue();
+        return ResponseEntity.ok(allProducts);
+    }
+
+    @PostMapping
+    public ResponseEntity registerProduct(@RequestBody @Valid RequestProduct data){
+     //data é apelido para a classe (na verdade o record RequestProduct pra ver se vem um name e um price_in_cents)
+        Product newProduct = new Product(data);
+        repository.save(newProduct);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping
+    @Transactional
+    public ResponseEntity updateProduct(@RequestBody @Valid RequestProduct data){
+        Optional<Product> optionalProduct = repository.findById(data.id());
+        if (optionalProduct.isPresent()) {
+            Product product = optionalProduct.get();
+            product.setName(data.name());
+            product.setPrice_in_cents(data.price_in_cents());
+            return ResponseEntity.ok(product);
+        } else {
+            throw new EntityNotFoundException();
+        }
+    }
+
+    //metodo delete para apagar o dado de fato
+//    @DeleteMapping("/{id}")
+//    public ResponseEntity deleteProduct(@PathVariable String id){
+//        repository.deleteById(id);
+//        System.out.println("deletado");
+//        return ResponseEntity.noContent().build();
+//    }
+
+    //serve para desativar o dado. Por exemplo, o produto nao esta disponivel no momento mas vai estar daqui um mes.
+    @DeleteMapping("/{id}")
+    @Transactional
+    public ResponseEntity deleteProduct(@PathVariable String id){
+        Optional<Product> optionalProduct = repository.findById(id);
+        if (optionalProduct.isPresent()) {
+            Product product = optionalProduct.get();
+            product.setActive(false);
+            return ResponseEntity.noContent().build();
+        } else {
+            throw new EntityNotFoundException();
+        }
+    }
+
+}
